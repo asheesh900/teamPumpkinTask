676833d55c9273853e059a8110efcec2bbbf31b5
asheesh900 <thebusinessman999@gmail.com>
2020-06-04 01:08:07 +0530

    ReadMe Update

--- Frontend/my-app/src/Components/Common/Home.jsx
+++ Frontend/my-app/src/Components/Common/Home.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Home page</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="homeImg">
+        <div>
+          Welcome to image database.
+          <br />
+        </div>
+        <Link to="/dashboard">
+          <button id="dash-btn">Dashboard</button>
+        </Link>
+      </div>
+    );
+  }
 }
---
+++ README.md
@@ -0,0 +1,20 @@
+#### Team Pumpkin Task
+
+### Technologies used
+
+### For Fron-end
+```
+React.js
+Redux
+React-Redux
+Redux-thunk
+Bootstrap
+CSS
+HTML
+```
+
+### For Back-end
+```
+Python Flask
+MySQL
+```
